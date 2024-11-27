495b4b80bb55e6994372fc84178b8c70417c6473
dagou <jeanchrist690@gmail.com>
2024-11-27 13:32:43 +0000

    Ajout des fichiers

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document |laravel </title>
+</head>
+<body>
+    <h2>petit twinner</h2>
+    <p class="first">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, ratione dolorum. Aliquid vero nobis blanditiis laudantium dolores architecto. Atque soluta minus iure aperiam quaerat eveniet. Amet quam adipisci expedita impedit.
+        Expedita quod numquam veniam nesciunt alias. Suscipit doloremque quasi inventore cumque sequi non quia fugiat beatae voluptates, ea magni commodi autem delectus voluptatem? Incidunt enim iusto quidem cupiditate sequi quis?
+        Soluta debitis possimus fugit, dolorem nam minus quas id error ipsam quod. Odit natus repellat fugit maxime. Autem, ratione, ut natus cupiditate reiciendis nisi mollitia consequatur tempora aperiam eveniet architecto.
+        Quibusdam provident expedita vero doloribus. Repudiandae fuga atque deleniti. Cumque ipsam doloribus quam consectetur maiores magnam laborum, dignissimos sequi libero exercitationem optio provident reiciendis doloremque minima! Minus nemo quod vitae.
+        Quia perferendis, quas eaque necessitatibus ab omnis quis magnam aliquid totam explicabo quibusdam. Eos recusandae quia dolore magni, eum quo consequatur veniam placeat repellendus! Ratione commodi officiis totam aspernatur asperiores.
+    </p>
+</body>
+</html>
